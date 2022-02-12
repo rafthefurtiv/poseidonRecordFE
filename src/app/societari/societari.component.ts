7746755bfb21b9a1af0f7ece5ac08777c90faa3a
Raffaele <rafthefurtiv@hotmail.it>
2022-02-12 20:05:04 +0100

    varie

--- src/app/societari/societari.component.ts
+++ src/app/societari/societari.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { RecordService } from '../record.service';
 import { RecordSocietario } from '../record-societario';
+import {forkJoin} from 'rxjs';
+import { Stili } from '../stili';
+import { Categorie } from '../categorie';
 
 
 @Component({
@@ -10,6 +13,14 @@ import { RecordSocietario } from '../record-societario';
 })
 export class SocietariComponent implements OnInit {
 
+  categoria: string = "";
+  metri: string = "";
+  stile: string = "";
+  sesso: string = "";
+  vasca: string = "";
+  categorie: Array<Categorie> = [];
+  stili: Array<Stili> = [];
+  metriOption = this.recordService.getMetriList();
   message: string = "";
   gare: Array<RecordSocietario> = [];
 
@@ -24,6 +35,48 @@ export class SocietariComponent implements OnInit {
    }
 
   ngOnInit(): void {
+        let categorieCall = this.recordService.getCategorieList();
+        let stiliCall = this.recordService.getStiliList();
+        let metriOption = this.recordService.getMetriList();
+
+        forkJoin(categorieCall, stiliCall).subscribe((result) => {
+          console.log(result);
+          this.categorie = result[0];
+          this.stili = result[1];
+        })
+  }
+
+  checkVisibility(gara: RecordSocietario){
+    let ricerca = false;
+    let takeGara = true;
+
+    if(this.metri != null && this.metri != undefined && this.metri != "" ||
+      this.stile != null && this.stile != undefined && this.stile != "" ||
+      this.categoria != null && this.categoria != undefined && this.categoria != ""
+    ){
+      ricerca = true;
+    }
+
+    if(ricerca){
+      if(this.metri != "" && !gara.gara.nomeGara.includes(this.metri)){
+        takeGara = false;
+      }
+
+      if(this.stile != "" && !gara.gara.nomeGara.includes(this.stile)){
+        takeGara = false;
+      }
+
+      if(gara.gara.categoria != this.categoria){
+        takeGara = false;
+      }
+
+        return takeGara;
+    }
+    else{
+      return true;
+    }
+
+
   }
 
 }
