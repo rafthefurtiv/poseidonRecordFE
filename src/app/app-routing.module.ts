d771fbdb09db869b42c996d85ee6a76362bfc31b
Raffaele <rafthefurtiv@hotmail.it>
2022-02-18 00:35:56 +0100

    varie

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,12 +5,14 @@ import { MenuComponent } from './menu/menu.component';
 import { AtletiComponent } from './atleti/atleti.component';
 import { SocietariComponent } from './societari/societari.component';
 import { AddRecordComponent } from './add-record/add-record.component';
+import { StartListComponent } from './start-list/start-list.component';
 
 const routes: Routes = [
 { path: '', component: LoginComponent},
 { path: 'menu', component: MenuComponent},
 { path: 'add-record', component: AddRecordComponent},
 { path: 'atleti', component: AtletiComponent},
+{ path: 'start-list', component: StartListComponent},
 { path: 'societari', component: SocietariComponent}
 ];
 
--- src/app/start-list/start-list.component.ts
+++ src/app/start-list/start-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { StartList } from '../start-list';
 
 @Component({
   selector: 'app-start-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./start-list.component.css']
 })
 export class StartListComponent implements OnInit {
+
+  fileList: Array<StartList> = [];
+  loading = false;
 
   constructor() { }
 
