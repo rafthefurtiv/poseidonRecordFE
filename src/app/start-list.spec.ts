1b1980fdadce6a3f49eb2cf85520f0236e38dfac
Raffaele <rafthefurtiv@hotmail.it>
2022-02-18 21:53:00 +0100

    varie

---
+++ src/app/start-list.spec.ts
@@ -0,0 +1,7 @@
+import { StartList } from './start-list';
+
+describe('StartList', () => {
+  it('should create an instance', () => {
+    expect(new StartList()).toBeTruthy();
+  });
+});
---
+++ src/app/start-list.ts
@@ -0,0 +1,6 @@
+export class StartList {
+
+  nome: String = "";
+  data: String = "";
+
+}
