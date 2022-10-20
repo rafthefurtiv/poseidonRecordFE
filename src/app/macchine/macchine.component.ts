4ddfe318bc93e9fbb905b92ce93d54e019870384
Raffaele Barbato <rafthefurtiv@gmail.com>
2022-10-20 13:07:10 +0200

    Header

--- src/app/macchine/macchine.component.ts
+++ src/app/macchine/macchine.component.ts
@@ -7,7 +7,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MacchineComponent implements OnInit {
 
-  constructor() { }
+  andata: Boolean = true;
+
+  constructor() {
+    //andata: Boolean;
+  }
+
+
 
   ngOnInit(): void {
   }
